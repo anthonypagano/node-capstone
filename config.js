59bb2c0b7503b71df5a4a4d03fedc245b0e244c4
Anthony Pagano <apagano51@gmail.com>
2018-12-30 16:15:45 -0500

    master: prod deployment fail adjustment

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 'use strict';
 
-exports.DATABASE_URL = 'mongodb://localhost:27017/albumDb'|| process.env.DATABASE_URL ;
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://apagano76:76apagano@ds263138.mlab.com:63138/node-capstone';
 //exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost:27017/testAlbumDb';
 exports.PORT = process.env.PORT || 8080;

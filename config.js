5555b1666b292489596d7183ec231fe813a73d71
Anthony Pagano <apagano51@gmail.com>
2019-01-02 19:52:11 -0500

    cleanup: added port number to path

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 'use strict';
-exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 'mongodb://localhost/albumDb';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/node-capstone-test';
+exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 'mongodb://localhost:27017/node-capstone';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost:27017/node-capstone-test';
 exports.PORT = process.env.PORT || 8080;
